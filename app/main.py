b34dbd2b96fcd8ac4aa7222311693d71381f9867
Shubham Karn <shubhamkarn@Shubhams-MacBook-Air.local>
2026-01-01 17:48:36 +0530

    nacg

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="College Event Portal API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://umang-2k25.netlify.app"
+        "https://thunderous-pastelito-0a2f09.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
